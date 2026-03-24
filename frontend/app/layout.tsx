f2aed51c1066b275f7133aa59dabcccc4f5406c3
아보카도즈 <avocadoz@abokadojeuui-MacBookPro.local>
2026-03-25 04:18:46 +0900

    fix metadata duplicate

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -6,33 +6,34 @@ export const metadata: Metadata = {
   description:
     "내 주변 종량제봉투, 불연성마대, 폐기물 스티커 판매처를 지도에서 빠르게 찾으세요.",
   keywords: ["종량제봉투", "불연성마대", "폐기물 스티커", "판매처", "쓰레기봉투"],
+
+  verification: {
+    google: "sLUNDaRp2dzRT9qid7DV_IDcnXHANrcuz1ULL",
+  },
+
   icons: {
     icon: "/Img/Icon/trash_bag_24.svg",
-    apple: "/Img/Icon/trash_bag_24.svg"
+    apple: "/Img/Icon/trash_bag_24.svg",
   },
+
   openGraph: {
     title: "종량제봉투 판매처 지도",
     description: "내 주변 쓰레기봉투 판매처 찾기",
     url: "https://vinyl-ochre.vercel.app",
-    siteName: "쓰레기봉투 지도",
+    siteName: "종량제봉투 지도",
     locale: "ko_KR",
-    type: "website"
-  }
+    type: "website",
+  },
 };
 
 export default function RootLayout({
-  children
-}: Readonly<{
+  children,
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="ko">
       <body>{children}</body>
     </html>
   );
 }
-export const metadata = {
-  verification: {
-    google: "sLuNDaRp2dzRT9qid7DV_IDcnXHANrcuz1ULL", // content 값만 넣기
-  },
-};
